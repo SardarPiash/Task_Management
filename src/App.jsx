42a5c59e629add787e20ce0840821ae99193176d
Sardar Piash <piashm66@gmail.com>
2024-04-03 02:37:15 +0600

    Html to Jsx Covert

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import TaskBoard from "./Taskboard/taskboard";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <div className=" flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+
+      <Footer />
+    </>
+  );
+}
